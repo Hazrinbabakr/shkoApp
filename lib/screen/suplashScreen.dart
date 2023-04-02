b4ae5cb9df1f3d5891619b4e922093d24ab3e606
Tawfik <twfek.kiki@gmail.com>
2023-04-02 19:26:55 +0300

    onboarding

--- lib/screen/suplashScreen.dart
+++ lib/screen/suplashScreen.dart
@@ -3,12 +3,13 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:onlineshopping/screen/on_boarding/onBoarding.dart';
+import 'package:onlineshopping/services/local_storage_service.dart';
 
 import 'auth/normal_user_login/login_main_page.dart';
 import 'homepage.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({ key}) : super(key: key);
+  const SplashScreen({key}) : super(key: key);
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -16,21 +17,20 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   bool activeConnection = false;
-String text = '';
+  String text = '';
+
   // ignore: non_constant_identifier_names
   Future CheckUserConnection() async {
-      final result = await InternetAddress.lookup('google.com');
-      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-        setState(() {
-          activeConnection = true;
-          print('true');
-        });
-      }
-      else {
-        activeConnection = false;
-        print('false');
-      }
-
+    final result = await InternetAddress.lookup('google.com');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      setState(() {
+        activeConnection = true;
+        print('true');
+      });
+    } else {
+      activeConnection = false;
+      print('false');
+    }
   }
 
   @override
@@ -39,66 +39,70 @@ String text = '';
     super.initState();
     CheckUserConnection();
 
-      Future.delayed(Duration(seconds: 2), () {
-        if(activeConnection==true){
-          Navigator.push(
-              context, MaterialPageRoute(builder: (crl) =>
-          OnBoardingPage()
-          // FirebaseAuth.instance.currentUser != null ?
-          // HomePage():
-          // MainLoginPage()
-          )
-          );
-        }
-        else{
-          setState(() {
-            text= 'Check Your Internet Connection';
-          });
-        }
-
-      });
-
-
+    Future.delayed(Duration(seconds: 2), () {
+      if (activeConnection == true) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (crl) =>
+                (LocalStorageService.instance.firstTime??true)
+                    ? OnBoardingPage()
+                    : FirebaseAuth.instance.currentUser != null
+                        ? HomePage()
+                        : MainLoginPage()
+            )
+        );
+      } else {
+        setState(() {
+          text = 'Check Your Internet Connection';
+        });
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-    //  backgroundColor:Color(0xff399642),
+      //  backgroundColor:Color(0xff399642),
       body: SafeArea(
         child: Container(
           height: height,
           decoration: BoxDecoration(
-            // image: DecorationImage(
-            //   //  alignment: Alignment.topCenter,
-            //     fit: BoxFit.cover,
-            //     image: AssetImage("assets/logo.png"))
-          ),
+              // image: DecorationImage(
+              //   //  alignment: Alignment.topCenter,
+              //     fit: BoxFit.cover,
+              //     image: AssetImage("assets/logo.png"))
+              ),
           child: Center(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: height / 3,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: height / 3,
+              ),
+              Center(
+                child: Container(
+                  child: Image.asset(
+                    'images/category/shkoLogo.png',
+                    width: width * 0.7,
                   ),
-                  Center(
-                    child: Container(
-                      child: Image.asset(
-                        'images/category/shkoLogo.png',
-                        width: width * 0.7,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  CircularProgressIndicator(
-                    backgroundColor: Theme.of(context).accentColor,
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
-                  ),
-                  SizedBox(height: 150,),
-                  Text(text)
-                ],
-              )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CircularProgressIndicator(
+                backgroundColor: Theme.of(context).accentColor,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Text(text)
+            ],
+          )),
         ),
       ),
     );
